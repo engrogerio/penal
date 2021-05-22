7e0f69fc229f662370e0a4c7755a32b4004bf800
Rogerio Silva <eng.rogerio@gmail.com>
2021-05-21 22:11:34 -0300

    bug fixes

--- analise.py
+++ analise.py
@@ -1,53 +1,92 @@
 import numeros
+import spacy
 
+def analysis(text: str):
+    nlp = spacy.load("pt_core_news_sm")
+    for doc in nlp.pipe(text): #, disable=["tok2vec", "tagger", "parser", "attribute_ruler", "lemmatizer"]):
+        # Do something with the doc here
+        print('Resultado=>', [(ent.text) for ent in doc.tag])
+        # print('Resultado=>', [(ent.text, ent.label_) for ent in doc.ents])
 
-with open('decreto.txt', 'r', encoding='utf-8') as decreto:
+def separate(text):
+    nlp = spacy.load("pt_core_news_sm")
+    tokens = nlp(text)
+    for sent in tokens.sents:
+        print(sent) #.string.strip())
+
+def get_text(file_name):
     lines = []
-    loop_paragraph = False
-    roman_numbers = ['I -', 'II -', 'III -', 'IV -', 'V -', 'VI -', 'VII -', 'VIII -', 'IX -', 'X -']
-    numbers = numeros.get_extenso()
-
+    fractions = get_fraction_numbers()
+    with open(file_name, 'r', encoding='utf-8') as text:
     # removing empty lines and spaces
-    for line in decreto:
-        if len(line.strip()) == 0:
-            continue # if line is empty, don't bring.
+        for line in text:
+            if len(line.strip()) == 0:
+                continue # if line is empty, don't bring.
 
-        # replace numbers in text (extenso) by digits
-        for k,v in numbers.items():
-            search = f' {v} ' 
-            if search in line:
-                line = line.replace(v, str(k)) 
-       
-        lines.append(line.strip())
-
-    for line in lines:
-        # if loop_paragraph:
-            
-        if any(line.startswith(x) for x in roman_numbers): 
-            print('Inciso ==>', line)
-        else:
-            loop_paragraph = False
-
-        if 'Art.' in line:
-            print('Artigo ==>', line)
-        if 'pena' in line.lower():
-            print('Pena ==>', line[line.lower().find('pena'):])
-
-        if 'aumentada' in line.lower():
-            print('Agravante ==>', line[line.lower().find('aumentada'):])
+            # replace numbers in text (extenso) by digits
+            numbers = numeros.get_extenso()
+            for k,v in numbers.items():
+                line = line.replace(f' {v} ', f' {k} ') 
  
-        if 'aumentar' in line.lower():
-            print('Agravante ==>', line[line.lower().find('aumentar'):])
+            # replace fraction number in text: um sexto => 1/6
+            for k,v in fractions.items():
+                line = line.replace(f'{v} ', f'/{k} ')
 
-        if 'diminuída' in line.lower():
-            print('Atenuante ==>', line[line.lower().find('diminuída'):])
+            lines.append(line.strip())
+
+    return lines
+
+def get_roman_numbers():
+    roman_numbers = [f'{n} -' for n in numeros.roman_numbers()]
+    return roman_numbers
+
+def get_fraction_numbers():
+    return numeros.get_fractions()
+
+def parse(line):
+    loop_paragraph = False
+    roman_numbers = get_roman_numbers()
+
+    line_result = ''
  
-        if 'reduzir' in line.lower():
-            print('Atenuante ==>', line[line.lower().find('reduzir'):])
+    if any(line.startswith(x) for x in roman_numbers): 
+        line_result += '\n\nInciso ==>' + line
+    else:
+        loop_paragraph = False
 
-        if any(x in line for x in ['§', 'Parágrafo']):
-            print('Parágrafo ==>', line)
-            loop_paragraph = True
+    if 'Art.' in line:
+        line_result += f'\n\nArtigo ==>' + line
+    if 'pena' in line.lower():
+        line_result += f'\n\nPena ==>' + line[line.lower().find('pena'):]
+
+    if 'aumentada' in line.lower():
+        line_result += f'\n\nAgravante ==>' + line[line.lower().find('aumentada'):]
+
+    if 'aumentar' in line.lower():
+        line_result += f'\n\nAgravante ==>' + line[line.lower().find('aumentar'):]
+
+    if 'diminuída' in line.lower():
+        line_result += f'\n\nAtenuante ==>' +  line[line.lower().find('diminuída'):]
+
+    if 'reduzir' in line.lower():
+        line_result += f'\n\nAtenuante ==>' + line[line.lower().find('reduzir'):]
+
+    if any(x in line for x in ['§', 'Parágrafo']):
+        line_result += f'\n\nParágrafo ==>' + line
+        loop_paragraph = True
+
+    if not line_result:
+        line_result += f'\n\nCrime ==>' + line
+    return line_result
+
+text_lines = get_text('decreto.txt')
+
+for n, line in enumerate(text_lines):
+    print(n, ' - ', line)
+    print(n, ' - ', parse(line))
+    print('\n=================================================================\n')
+
+
 
 test_json = {
     "name": "Código Penal", "version": "2020-01-13",
--- numeros.py
+++ numeros.py
@@ -1,19 +1,27 @@
-
-
-extenso = ['um', 'dois', 'três', 'quatro', 'cinco', 'seis', 
+def get_fractions():
+    fractions = ['um', 'terço', 'quarto', 'quinto', 'sexto']
+    numbers = [1, 3, 4, 5, 6]
+    return dict(zip(numbers, fractions))
+    
+def get_extenso() -> dict:
+    """
+    Returns a dictionary with numbers in portuguese text. Ex:{1: 'Um'}
+    """
+ 
+    extenso = ['um', 'dois', 'três', 'quatro', 'cinco', 'seis', 
         'sete', 'oito', 'nove', 'dez', 'onze', 'doze', 'treze',
         'quatorze', 'quinze', 'dezesseis', 'desessete', 'dezoito',
         'dezenove', 'vinte', 'vinte e um', 'vinte e dois', 'vinte e três',
         'vinte e quatro', 'vinte e cinco', 'vinte e seis', 'vinte e sete',
         'vinte e oito', 'vinte e nove', 'trinta']
-numbers = range(1, 31)
+    numbers = range(1, 31)
 
-def get_extenso() -> dict:
-    """
-    Returns a dictionary with numbers in portuguese text. Ex:{1: 'Um'}
-    """
     return dict(zip(numbers, extenso))
 
+def roman_numbers():
+
+    roman_numbers = ['I', 'II', 'III', 'IV', 'V', 'VI', 'VII', 'VIII', 'IX', 'X']
+    return roman_numbers
 
 
 
