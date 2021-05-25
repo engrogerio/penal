1bb927251ca56a91a0fbe469bc3208524d3ad030
Rogerio Silva <eng.rogerio@gmail.com>
2021-05-25 18:28:44 -0300

    choose capitulo done

--- analise.py
+++ analise.py
@@ -19,6 +19,8 @@ def get_text(file_name) -> list:
             # for k,v in fractions.items():
                 # line = line.replace(f'{v} ', f'/{k} ')
 
+            # replace all – by -
+            line = line.replace('–', '-')
             lines.append(line.strip())
 
     
@@ -66,7 +68,7 @@ def classify(line):
     result = []
     # referencia
     if 'Lei nº ' in line:
-        defined = True
+        # defined = True
         result.append('referencia')
     # parte
     if 'PARTE' in line:
@@ -93,7 +95,7 @@ def classify(line):
         defined = True
         result.append('paragrafo')
     # pena
-    if 'Pena - ' in line:
+    if any(x in line for x in ['Pena - ', 'Pena: ']):
         defined = True
         result.append('pena')
         
@@ -103,7 +105,7 @@ def classify(line):
         result.append('atenuante')
     
     # agravante 
-    if any(x in line.lower() for x in ['aumentada', 'aumentar', 'aumentam']) and 'pena' in line.lower():
+    if any(x in line.lower() for x in ['aumentada', 'aumentar', 'aumentam', 'aumenta-se']) and 'pena' in line.lower():
         defined = True
         result.append('agravante')
     
@@ -111,14 +113,23 @@ def classify(line):
     if any(line.startswith(f'{x} - ') for x in get_roman_numbers()): 
         defined = True
         result.append('inciso')
+        
     # alinea
     if re.search('^[a-z]\) ', line):
         defined = True
         result.append('alinea')
+    
+    # vazio
+    if line == '':
+        defined = True
+        result.append('vazio')
+    
     # tipo
     if not defined:
-        if ' de pena' not in line:
-            result.append('tipo')
+        if ' de pena' not in line and ' da pena' not in line:
+            if not re.search('^\(.*\)', line):
+                result.append('tipo')
+            
     return result
 
 def get_all_entitie_lines(text_lines, entity):
@@ -128,7 +139,7 @@ def get_all_entitie_lines(text_lines, entity):
     lines = []
     for n, line in enumerate(text_lines):
         classification = classify(line)
-        if line !='' and all(x in classification for x in [entity]):
+        if all(x in classification for x in [entity]):
             lines.append(n)
     return lines
 
@@ -139,16 +150,26 @@ def apply_corrections(text_lines):
     # replace line 3892 by line 3894 and vice versa
     text_lines[3892], text_lines[3894] = text_lines[3894], text_lines[3892]
     
+    # replace line 3919 by line 3920 and vice versa
+    text_lines[3919], text_lines[3920] = text_lines[3920], text_lines[3919]
+
+    #lines 2476 replace 2478 and vice versa
+    text_lines[2476], text_lines[2478] = text_lines[2478], text_lines[2476]
+    
     # replace typo 1o by 1º on line 1870
     text_lines[1869] = text_lines[1869].replace('1o', '1º')
 
+
+    # replace typo : by - on line 3677
+    text_lines[3677] = text_lines[3677].replace(':', ' -')
+
     # removing lines 3553 and 3554
-    text_lines[3552] = ''
-    text_lines[3553] = ''
-    
     # line 3554 should be all caps
-    text_lines[3554] = text_lines[3554].upper()
+    text_lines[3552] = text_lines[3554].upper()
+    text_lines[3553] = ''
+    text_lines[3554] = ''
     
+
     # line 1261 missing dash
     text_lines[1261] = 'III - ' + text_lines[1261][4:]
     
@@ -208,28 +229,98 @@ def capitulos(text_lines):
             lines[line]["name"] = text_lines[n+1]
     return lines
 
-text_lines = get_text('decreto.txt')
-text_lines = apply_corrections(text_lines)
-
-lines = []
-count = 0
-for n, line in enumerate(text_lines):
-    classification = classify(line)
-    if line != '' and any(x in classification for x in ['secao', 
-        'titulo', 'capitulo', 'parte', 'tipo' ]):
-        count += 1
+def get_tag_end_line(text_lines: list, start_line: int, tag: str) -> int:
+    """
+    Returns the line which has the same tag as the start.
+    Ex. user requests an Article in line 2000. This method returns
+    a line number where is the next Article starting from line 2000
+    which is where the requested Article ends.
+    
+    :param tag: One of the tags: 
+    # parte, titulo, capitulo, seção, artigo, paragrafo,
+    # pena, atenuante, agravante, inciso, alinea, tipo
+    """
+    # get next tag line number with the same type 
+    # ex. start = capitulo I , end = capitulo II
+    lines = get_lines_by_tag(text_lines[start_line:], [tag,])
+    # Lets grab the line before the next tag start (-1)
+    end_line = lines[1][1][0] - 1
+    return end_line
+    
+def apply_classification(text_lines) -> list:
+    """
+    Returns a list of tuples with:
+    (law line number, law text, list of tags)
+    
+    """
+    lines = []
+    count = 0
+    for n, line in enumerate(text_lines):
+        classification = classify(line)
         item = (n, line, classification)
         lines.append (item)
-        print(count, ' - ', item[0], ' - ', item[1], ' - ', item[2])
-        print('==================================================')
-print(f'{count} ocorrências')
+    return lines
+
+def get_lines_by_tag(text_lines, tags: list) -> list:
+    """
+    Returns a list of tuples with:
+    (counter, law line number, law text, list of tags)
+    based on the tags passed as parameter.
+    """
+    lines = []
+    count = 0
+    for n, line in enumerate(text_lines):
+        classification = line[2]
+        if any(x in classification for x in tags):
+            count += 1
+            item = (count, line)
+            lines.append (item)
+    return lines
+
+# main ######
+
+text_lines = get_text('decreto.txt')
+text_lines = apply_corrections(text_lines)
+text_lines = apply_classification(text_lines)
+
+lines = get_lines_by_tag(text_lines, ['secao', 'titulo', 
+                                    'capitulo', 'parte'])
+for line in lines:
+    print(line) #[0], ' - ', line[1], '-', line[2], ' - ', line[3])
+    print('==================================================')
 
 option = input('Entre o número do ítem : ')
-start = lines[int(option)+1][0]
-end = lines[int(option)+2][0]
-print(item[1], start, '->', end)
-for line in text_lines:
-    if line != '':
-        pass
-        # print(line)
-        # print('\n================================\n')
+opt = int(option)
+# grab the exact line data
+item = lines[opt-1]
+item_count = item[0]
+item_line_number = item[1][0]
+item_content = item[1][1]
+item_tags = item[1][2]
+print(item_content)
+# get the line start and end for the choosed item
+start = item_line_number
+
+# show up first
+# grab the last item_tag (-1) due to the tag "reference" 
+# appears always as the first item
+end = get_tag_end_line(text_lines, start, item_tags[-1]) 
+# print ('&&&&', start, item_tags[-1], end)
+count = 0
+for line in text_lines[start: end]:
+    if any(x in line[2] for x in ['tipo', 'capitulo', 'artigo', 'paragrafo', 
+                 'inciso', 'alinea']): 
+        count += 1
+        print(count, ' - ', line)
+
+option = input('Entre o número do ítem : ')
+opt = int(option)
+# grab the exact line data
+item = lines[opt-1]
+item_count = item[0]
+item_line_number = item[1][0]
+item_content = item[1][1]
+item_tags = item[1][2]
+
+print(text_lines)
+
